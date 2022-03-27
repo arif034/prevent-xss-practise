68b1a9e937e25e1c41c735960bac72cc8e1a9adb
arif034 <45455917+arif034@users.noreply.github.com>
2022-03-27 19:56:00 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="App">Counter</h1>
+      <h2 className="value">{}</h2>
+    </div>
+  );
+}
+export default App;
